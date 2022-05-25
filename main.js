11286eadc42321dec32171d1b50ec365460bf016
Gabryel <gabryel.com.br@gmail.com>
2022-05-25 16:42:56 -0300

    Finalização completa da página

--- main.js
+++ main.js
@@ -8,6 +8,51 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+  // linha alvo
+  const targetLine = scrollY + innerHeight / 2
+
+  // verificar se a seção passou da linha
+  // quais dados vou precisar?
+
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+
+  // o topo da seção chegou ou ultrapassou a linha alvo
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+  // informações dos dados e da lógica
+  console.log(
+    'o topo da seção chegou ou passou da linha?',
+    sectionTopReachOrPassedTargetLine
+  )
+
+  //verificar se a base está abaixo da linha alvo
+  //quais dados vou precisar?
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+  //console.log('o fundo da sessão passou da linha?', sectionEndPassedTargetLine)
+
+  // limites da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  const sectionId = section.getAttribute('id')
+
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
 }
 
 function showNavOnScroll() {
@@ -38,7 +83,7 @@ function closeMenu() {
 ScrollReveal({
   origin: 'top',
   distance: '30px',
-  duration: 700,
+  duration: 700
 }).reveal(`
   #home, 
   #home img, 
@@ -54,3 +99,7 @@ ScrollReveal({
   #contact ul,
   #contact a,
   #contact img`)
+
+function sayMyName(name) {
+  console.log(name)
+}
